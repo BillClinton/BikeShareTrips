866bf71487881adb08d55cccfcb6a20832ee4734
Bill Clinton <bclinton@appunto.net>
2024-01-31 02:52:22 -0500

    refactor: component in the middle no longer needed

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-import Map from './Map.vue'
+import { onMounted, ref } from 'vue'
+import tripsData from '@/assets/trips2324.json'
+import TripMap from '@/components/TripMap.vue'
+
+interface Trip {
+  start: { lat: number; lng: number }
+  end: { lat: number; lng: number }
+}
+
+let trips = ref<Trip[] | null>(null)
+
+onMounted(async () => {
+  trips.value = tripsData.map((trip) => ({
+    start: { lat: trip.checkOutLat, lng: trip.checkOutLon },
+    end: { lat: trip.checkInLat, lng: trip.checkInLon }
+  }))
+})
 </script>
 
 <template>
   <Suspense>
     <!-- component with nested async dependencies -->
-    <Map />
+    <TripMap v-if="trips" :trips="trips" />
 
     <!-- loading state via #fallback slot -->
     <template #fallback> Loading... </template>
